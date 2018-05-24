92bb48465a9a7281a07798399dc9983a697ccc9d
Matias Cantoni <matias.cantoni96@gmail.com>
2018-05-24 10:08:30 -0300

    Initial Commit

---
+++ 01-variables-operadores/app.js
@@ -0,0 +1,54 @@
+//Comentario de 1 linea
+/*
+	Comentario
+	Multiples 
+	Lineas
+*/
+'use strict'
+
+var a;
+var A;
+var b = 10;
+console.log(a, A, b, text);
+a = 100;
+A = 99;
+var text = 'hi';
+console.log(a);
+alert(text);
+console.log(A);
+alert(b);
+
+//Aritmethic
+var c = a + 200 + 100;
+console.log(c);
+c = (100 + 50) / 2 * 10;
+console.log(c);
+var mod = 45 % 7;
+console.log(mod);
+
+//Comparission
+console.log(a > A);
+console.log(a < A);
+console.log(a > a);
+console.log(a >= a);
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 != false);
+console.log(0 !== false);
+
+//Boolean
+var t = true;
+var f = false;
+console.log(t && t);
+console.log(t && f);
+console.log(f && f);
+console.log(t || t);
+console.log(t || f);
+console.log(f || f);
+console.log(!t);
+console.log(!f);
+
+//String
+var s = 'hello';
+text = s + 'bye';
+console.log(text);
